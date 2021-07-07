75de93238c6e0e9a297139d120a96e3154954dc2
Joshua Jones <JJones@internal.cubase.org>
2021-07-07 09:35:59 -0400

    Updates

--- src/events/signatures-set-event.ts
+++ src/events/signatures-set-event.ts
@@ -4,10 +4,6 @@ export interface SignaturesSetEvent {
   subject: Subjects.SignaturesSet;
   data: {
     userId: string;
-    signaturetype: string;
-    signature: string;
-    initialstype: string;
-    initials: string;
     version: number;
   };
 }
